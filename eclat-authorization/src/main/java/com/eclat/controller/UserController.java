efe49a1090d3ec1ac3f23fe39572190a14d46579
furqann <furqanahmad21@hotmail.com>
2020-08-12 16:07:56 +0500

    implemented the UserManagementApi into the UserController

--- eclat-authorization/src/main/java/com/eclat/controller/UserController.java
+++ eclat-authorization/src/main/java/com/eclat/controller/UserController.java
@@ -1,5 +1,17 @@
 package com.eclat.controller;
 
-public class UserController {
+import com.eclat.api.UserManagementApi;
+import com.eclat.api.model.UserModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController implements UserManagementApi {
+
+  @Override
+  public ResponseEntity<UserModel> findUserById() {
+    return ResponseEntity
+        .ok(new UserModel().name("Test User").email("test@eclat.com").username("test_user"));
+  }
 
 }
